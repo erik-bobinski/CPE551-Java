145ea16525f6d55ffd7af44d330e267e503bf053
Erik <ebobinski0@gmail.com>
2024-10-21 21:13:10 -0400

    Project1 complete

--- Project1/BookMobile.py
+++ Project1/BookMobile.py
@@ -2,7 +2,74 @@ import BookMobileFunctions as bmf
 
 
 def main():
-    print("the main function")
+    
+    isFilesLoaded = False
+
+    bmf.Welcome()
+
+
+    while True:
+
+        choice = bmf.Menu()
+
+        match choice:
+
+            case 1:
+                bookDict, categoriesSet = bmf.LoadBooks()
+                isFilesLoaded = True
+
+            case 2:
+                if not isFilesLoaded:
+                    print("You need to load Book Files!\n")
+                else:
+                    try:
+                        reviewList = bmf.LoadReviews(bookDict)
+                        isFilesLoaded = True
+                    except LookupError:
+                        print("File does not exist!\n")
+
+            case 3:
+                if not isFilesLoaded:
+                    print("You need to load Book Files!\n")
+                else:
+                    bmf.BooksFromCategory(bookDict, categoriesSet)
+
+            case 4:
+                if not isFilesLoaded:
+                    print("You need to load Book Files!\n")
+                else:
+                    bmf.BookData(bookDict, reviewList)
+
+            case 5:
+                if not isFilesLoaded:
+                    print("You need to load Book Files!\n")
+                else:
+                    bmf.AuthorRatings(bookDict, reviewList)
+
+            case 6:
+                if not isFilesLoaded:
+                    print("You need to load Book Files!\n")
+                else:
+                    bmf.HelpfulReviewer(reviewList)
+
+            case 7:
+                exit()
+
+            case _:
+                print("Invalid option, choose between [1-7]")
+
+    #bookDict, categoriesSet = bmf.LoadBooks() 
+
+    #reviewList = bmf.LoadReviews(bookDict) 
+
+    #bmf.BooksFromCategory(bookDict, categoriesSet)
+
+    #bmf.BookData(bookDict, reviewList)
+
+    #bmf.AuthorRatings(bookDict, reviewList)
+
+    #bmf.HelpfulReviewer(reviewList)
+
 
 if __name__ == "__main__":
     main()
--- Project1/BookMobileFunctions.py
+++ Project1/BookMobileFunctions.py
@@ -1,29 +1,37 @@
-from os.path import exists # validate file existence
-from typing import Dict, Set, Tuple # to specify return types - type safety and paramter organization
+import os # validate file existence
 
-def LoadBooks() -> Tuple[ Dict[str, list[str]], Set[str] ]:
+from typing import Dict, Set, Tuple # to specify return types -> type safety and paramter organization
+
+# stying
+italic = "\033[3m"
+reset = "\033[0m"
+
+def LoadBooks() -> Tuple[Dict[str, list[str]], Set[str]]:
     """
-    Reads in csv file to extract book data, returns dictionary of data, and set of book categories
-
+    Reads in csv file of book data, returns dict of data and set of categories
     :return: Tuple containing dictionary of books, and set of categories
     """
 
     books = {} # key: book title, value: data
     categories = set() # tracks categories for books
-    bookFile = input("Input the csv file containing book information: ")
-
+    
     while True:
-
-        # check file existence and parse 
-        if exists(bookFile):
+        # specify path to target file
+        projectDir = os.path.dirname(os.path.abspath(__file__))
+        sampleDir = os.path.join(projectDir, "SampleInputOutput")
+        bookFile = os.path.join(sampleDir, input("Input the csv file containing book information: ")) # finalized path
+        
+        # check file existence and parse
+        if os.path.exists(bookFile):
             with open(bookFile, "r") as file:
                 next(file) # skip first line (header line)
                 for line in file:
                     lineList = line.strip().split(",") # break line into comma-delimited list
                     books[lineList[0]] = lineList[1:] # book title as key, remaining data as value
-                    categories.add( lineList[-1].lower() ) # add category(last list item) to set 
+                    categories.add( lineList[-1].lower() ) # add category(last list-item) to set 
             break
-        bookFile = input("File not found. Input the csv file containing book information: ")
+        else:
+            print("File not found.\n")
     
     return books, categories
 
@@ -31,7 +39,7 @@ def LoadBooks() -> Tuple[ Dict[str, list[str]], Set[str] ]:
 
 def LoadReviews(books: Dict[ str, list[str] ]) -> list[str]:
     """
-    Reads in csv file to extract book review data, returns list of book reviews 
+    Reads in csv file to extract book review data, returns list of book review data 
     
     :param books: contains book titles and corresponding data
     :param type: Dict[ str, list[str] ]
@@ -39,38 +47,42 @@ def LoadReviews(books: Dict[ str, list[str] ]) -> list[str]:
     :return: list of reviews of books found in book dictionary
     """
 
-    # list of book reviews that exist in book dict
+    # result list to build
     bookReviews = [] 
     
-    reviewFile = input("Input the csv file of the book reviews: ")
-
     while True:
+        # specify path to target file
+        projectDir = os.path.dirname(os.path.abspath(__file__))
+        sampleDir = os.path.join(projectDir, "SampleInputOutput")
+        reviewFile = os.path.join(sampleDir, input("\nInput the csv file containing book review information: ")) # finalized path
+
         
         #check file existence and parse
-        if exists(reviewFile):
+        if os.path.exists(reviewFile):
             with open(reviewFile, "r") as file:
+                next(file) # skip first line (header line)
                 for line in file:
-                    next(file) # skip first line (header line)
                     lineList = line.strip().split(",") # break line into comma-delimited list
 
                     try:
-                        # check if book is in dictionary
+                        # check if title in dict
                         if lineList[1] in books:
-                            # add book's review to list of reviews
-                            bookReviews.append(lineList[5])    
+                            # add review to list of reviews
+                            bookReviews.append(lineList)    
                         else:
                             raise LookupError(f"{lineList[1]} not found in books dictionary")
 
                     except LookupError: 
                         print(LookupError)
-            break
-        reviewFile = input("File not found. Input the csv file of the book reviews: ")
+                break
+        else:
+            print("File not found.")
 
     return bookReviews
 
 
 
-def BooksFromCategory(books: Dict[ str, list[str] ], categories: Set[str]) -> None:
+def BooksFromCategory(books: Dict[str, list[str]], categories: Set[str]) -> None:
     """
     Outputs all books from user-specified category
 
@@ -82,37 +94,39 @@ def BooksFromCategory(books: Dict[ str, list[str] ], categories: Set[str]) -> No
 
     :return: None
     """
-    
+    # list conversion of categories-set, for easier traversal 
+    categoriesList = list(categories)
+
     userCategory = ""
 
-    # input must exist in categories and be of type int
-    while userCategory not in categories or not isinstance(userCategory, int):
+    # user input must exist in categories and be of type int
+    while not isinstance(userCategory, int):
 
         # Display all categories for user to choose from
-        print("Below are all book categories:")
-        count = 1 # index of categories set
-        for category in categories:
-            print(f"{count}. {category}")
-            count += 1
-        userCategory = input(f"\nWhich category would you like? (1 - {len(categories)})") 
+        print("\nBelow are all book categories:")
+        for i in range(len(categoriesList)):
+            print(f"\t{i+1}. {categoriesList[i]}")
 
-    print(f"Each book below is of the {userCategory} category.")
+        # error-handle user input
+        try:
+            userCategory = int( input(f"\nWhich category would you like to see? (1 - {len(categoriesList)}): ") ) 
+        except:
+            print("You must input an integer within the specified bounds!")
+
+    print(f"{categoriesList[userCategory - 1]} books:")
 
     for title in books:
-        # if book has that userCategory
-        if books[title][len(books[title]) - 1] == userCategory:
-            print(f"\tTitle: \"{title}\"\tAuthor: {books[title][1]}") # title is dict key, index 1 is author
+        # if book has the user specified category
+        if books[title][-1].lower() == categoriesList[userCategory - 1]:
+            print(f"\t{italic}{title}{reset}, by {books[title][1]}\n") # title is dict key, index 1 is author
 
 
 
-def BookData(book: Dict[str, list[str]], bookReviews: list[str]):
-
-    # function instructions on end of page 2 of project instructions 
-    # debug prev functions before writing this one
+def BookData(books: Dict[str, list[str]], bookReviews: list[str]) -> None:
     """
     Choose a book to output all its data
 
-    :param book: collection of all books
+    :param books: collection of all books
     :param type: Dict[str, list[str]]
 
     :param bookReviews: list of all book reviews
@@ -120,3 +134,190 @@ def BookData(book: Dict[str, list[str]], bookReviews: list[str]):
 
     :return: None
     """
+    booksList = [] # list of book titles
+    userIndex = "" 
+
+    # ensure user-chosen book exists and is inputted as integer from a list
+    while not isinstance(userIndex, int):
+
+        # build booksList and show titles to user
+        print("\nHere are all available book titles:")
+        count = 1
+        for title in books:
+            print(f"\t{count}. {italic}{title}{reset}")
+            booksList.append(title)
+            count += 1
+
+        try:
+            userIndex = int( input(f"\nWhich book would you like to see?(1 - {count - 1}): " ) ) # 1-index of booksList
+        except:
+            print("You must input an integer within the above bounds!")
+            userIndex = ""
+            continue
+        
+        if booksList[userIndex - 1] not in books:
+            print("Book not found!")
+            userIndex = ""
+            continue
+
+    #scan review list for ratings and pricings of user-chosen book
+    userBook = booksList[userIndex - 1] # title of user-chosen book
+    priceSum = 0
+    ratingSum = 0
+    bookCount = 0
+    for line in bookReviews:
+        if line[1] == userBook:
+            priceSum += float(line[2]) 
+            ratingSum += float(line[-1])
+            bookCount += 1
+
+    # calc averages if a review exists
+    if bookCount != 0:
+        priceAvg = priceSum / bookCount
+        ratingAvg = ratingSum / bookCount
+        priceAvg = round(priceAvg, 2)
+        ratingAvg = round(ratingAvg, 1)
+
+    #output detailed info on userBook
+    print(f"\nMore information on {italic}{userBook}{reset}:")
+    print(f"\tDescription: ", end="")
+    print("-\n\t".join([books[userBook][0][i:i+60] for i in range(0, len(books[userBook][0]), 60)]))  # Breaks every 60 characters
+    print(f"\n\tPublished by: {books[userBook][2]}, {books[userBook][3]}")
+    print(f"\n\tCategory: {books[userBook][-1]}")
+    if bookCount != 0: # if user-chosen book has reviews
+        print(f"\n\tAverage price: ${priceAvg}")
+        print(f"\n\tAverage rating: {ratingAvg} / 5.0\n")
+    else:
+        print("\n\tNo price listings nor reviews available\n")
+
+
+
+def AuthorRatings(books: Dict[str, list[str]], bookReviews: list[str]) -> None:
+    
+    # build dict of {authors:their book titles} using books dict
+    authorBooks = {}
+
+    # add each author and their title's from books to authorBooks
+    for title in books:
+        
+        # handle case of multiple authors for one book
+        authors = books[title][1].split(";")
+        
+        for author in authors:
+
+            # if author not in authorBooks, initialize the author
+            if author not in authorBooks:
+                authorBooks[author] = [title]        
+
+            # else add new title to the author
+            else:
+                authorBooks[author].append(title) 
+
+    # determine average rating of each author using authorBooks and bookReviews 
+    authorRating = {} # dict {author:average rating}
+
+    for author in authorBooks:
+        ratingSum = 0.0
+        ratingCount = 0
+
+        for review in bookReviews:
+
+            for book in authorBooks[author]:
+                
+                if book in review:
+                    ratingCount += 1 # increment amount of ratings for the author
+                    ratingSum += float(review[-1]) # add rating to total
+                    authorRating[author] = ratingSum / ratingCount # insert latest average for author
+        if ratingCount == 0:
+            authorRating[author] = "N/A"
+
+
+    # print result
+    print(f"\nEach author and their average ratings:")
+    for author in authorRating:
+        if authorRating[author] != "N/A":
+            print(f"\n\t{author}: {float(authorRating[author]):.2f}")
+        else:
+            print(f"\n\t{author}: No ratings")
+
+
+def HelpfulReviewer(bookReviews: list[str]) -> None:
+    
+    # create dict {reviewer:helpfulness[numerator, denominator]}
+    # numerator = amount of ratings that stated a reviewer was helpful
+    # denominator = total amount of ratings of a reviewer
+    revRatings = {} 
+
+    for review in bookReviews:
+
+        reviewer = review[4]
+
+        # convert helpfulness ratings into list of ints for analysis
+        # [0] = numerator, [1] = denominator
+        helpRating = review[-2].split("/")
+        helpRating = [int(num) for num in helpRating]
+    
+        # sum up total helpfulness ratings
+        if reviewer not in revRatings:
+            revRatings[reviewer] = helpRating
+        else:
+            # add the two fractions
+            revRatings[reviewer] = [num + denom for num, denom in zip(revRatings[reviewer], helpRating)]
+            revRatings[reviewer][1] += helpRating[1]
+
+
+    # find most helpful reviewer by iterating thru revRatings{reviewer: [num, denom]}
+
+    # attributes of most helpful reviewer
+    maxNumer = 0
+    maxDenom = 0
+    maxName = "" 
+
+    for reviewer in revRatings:
+        numer = revRatings[reviewer][0] # amount of meta-ratings that stated helpful
+        denom = revRatings[reviewer][1] # total amount of meta-ratings
+
+        # only consider reviewers with at least 10 meta-ratings
+        if denom > 10:
+            
+            # we found a new most helpful reviewer
+            if numer > maxNumer:
+                maxNumer = numer
+                maxDenom = denom
+                maxName = reviewer 
+
+    # print result
+    maxAvg = int(maxNumer / maxDenom * 100)
+    print(f"\nMost helpful reviewer: {maxName}\nAverage Helpfulness Rating: {maxAvg}%\n")
+
+
+
+def Welcome() -> None:
+    print("Welcome to Book Mobile :^)\n")
+
+
+
+def Goodbye() -> None:
+    print("Goodbye -.-")
+
+
+
+def Menu() -> int:
+    num = ""
+    while not isinstance(num, int): 
+
+        try:
+            num = int( input(
+                "\nWhich option would you like to choose? (1 - 7)\n"
+                "1. Load Book File\n"
+                "2. Load Book Review File\n"
+                "3. Show Books by Literary Category\n"
+                "4. Show Book's Details\n"
+                "5. Show Average Author Ratings\n"
+                "6. Most Helpful Reviewer\n"
+                "7. Quit\n\n") )
+        except:
+            print("\nEnter an integer [1-7]")
+
+    return num
+     
